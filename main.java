5ee7e7c9a3ac88a97f41e028a04115995c77fd6f
kchowhan <kchowhan@expedia.com>
2019-12-10 19:49:23 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class JavaStatsEx {
+
+    /**
+     * Application entry point.
+     *
+     * @param args application command line arguments
+     */
+    public static void main(String[] args) {
+
+        var app = new MyStatsApp();
+        app.run(args);
+    }
+}
